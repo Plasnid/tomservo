2613e3891cb01de91413ac8500fb90eda3463368
Stuart Sackler <stuart@sackler.ca>
2025-03-03 21:30:16 -0500

    dual servo motion with reset

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ function mover () {
     servos.P1.setAngle(150)
     servos.P0.setAngle(90)
     basic.pause(1000)
-    reset();
+    reset()
 }
-function reset(){
+function reset () {
     servos.P0.setAngle(0)
     servos.P1.setAngle(0)
 }

75d216660edf58f10b05ed5dda4a770ba846b58b
Stuart Sackler <stuart@sackler.ca>
2025-03-03 20:53:55 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+function mover () {
+    servos.P1.setAngle(150)
+    servos.P0.setAngle(90)
+    basic.pause(1000)
+    reset();
+}
+function reset(){
+    servos.P0.setAngle(0)
+    servos.P1.setAngle(0)
+}
+mover()
